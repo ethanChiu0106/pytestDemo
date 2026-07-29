0c4ce73fec21754c76a6f35e05b4fdae4f54526c
Ethan <yiyu.chiu0106@gmail.com>
2026-07-29 16:49:56 +0800

    test(register): 補上「帳號已存在」的前置 fixture
    該案例原本靠「註冊成功」案例先跑過才會通過，單獨執行會失敗。改由
    fixture 預先註冊，並以 expected 而非案例 id 判斷是否需要前置。

--- testcases/api_test/http/test_register.py
+++ testcases/api_test/http/test_register.py
@@ -2,11 +2,26 @@ import pytest
 
 from api.auth import AuthAPI
 from test_data.api_test_data.http.register import RegisterCase, generate_register_cases
+from test_data.common.expectations import HTTP
 from utils.case_verify_tool import verify_case_auto
+
+
+@pytest.fixture
+def seed_existing_account(case: RegisterCase, auth_api: AuthAPI):
+    """為「帳號已存在」案例預先註冊帳號，使它不必依賴「註冊成功」案例先跑過
+
+    Args:
+        case: 當前的註冊測試案例。
+        auth_api: 匿名的 AuthAPI client，註冊本身不需授權。
+    """
+    if case.expected['result'] != HTTP.Auth.Register.REPEATED_ACCOUNT:
+        return
+    auth_api.register(case.request.account, case.request.password)
 
 
 class TestRegister:
     @pytest.mark.parametrize('case', generate_register_cases())
+    @pytest.mark.usefixtures('seed_existing_account')
     def test_register(self, case: RegisterCase, auth_api: AuthAPI):
         request = case.request
         expected = case.expected
